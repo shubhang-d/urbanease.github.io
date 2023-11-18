17638013746450440e6d51bdfd5e58a395f2e24b
Shubhang <Shubhangd205@gmail.com>
2023-11-18 16:46:26 +0530

    addded sidebar on cart

--- screens-js/index.js
+++ screens-js/index.js
@@ -5,3 +5,8 @@ function closeSidebar(){
 function openNav(){
     document.getElementById("sidebarOverlay").style.visibility='visible';
 }
+
+function scrolltoview(){
+    const elem = document.getElementById("sec-categories");
+    elem.scrollIntoView({behavior: "smooth", block: "end"});
+}
